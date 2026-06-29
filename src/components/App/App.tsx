3f54c732d6ebf1bef26db84e98209cb99358abad
champix56 <desorbaix@free.fr>
2026-06-29 12:26:27 +0200

    meo de button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,12 @@
+import Button from "../uis/Button/Button";
+
 const App = () => {
-  return <p>DEMAT BREIZH</p>;
+  return (
+    <>
+      <p>DEMAT BREIZH</p>
+      <Button />
+    </>
+  );
 };
 
-export default App
+export default App;

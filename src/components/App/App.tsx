0246471cfa22b43b76933e87d938019034525714
champix56 <desorbaix@free.fr>
2026-06-29 16:38:43 +0200

    fin button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,22 @@
+import B2 from "../uis/B2/B2";
 import Button from "../uis/Button/Button";
 
 const App = () => {
   return (
     <>
       <p>DEMAT BREIZH</p>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button
+        color="green"
+        onCliked={() => {
+          console.trace("toto");
+        }}
+      >
+        OK
+      </Button>
+      <Button onCliked={() => console.log("cancel")} color="green">
+        <img src="favicon.svg" alt="" />
+        Cancel
+      </Button>
     </>
   );
 };
--- src/components/uis/Button/Button.tsx
+++ src/components/uis/Button/Button.tsx
@@ -1,8 +1,29 @@
-import style from './Button.module.css';
-const Button = () => {
-  return (
-    <div className={style.Button} >Button</div>
-  )
+import style from "./Button.module.css";
+interface IButtonProps {
+  color?: "tomato" | "skyblue" | "green";
+  type?:'submit'|'reset'|'button';
+  onCliked?: () => void;
+  children:React.ReactNode|Array<React.ReactElement|string>
 }
+/**
+ * bouton avec gestion de formulaire pour validation ou onclick
+ * @param param0 IButtonProps props du composant
+ * @returns React.ReactNode constitution du composant de button
+ */
+const Button = ({type='button',children,color,onCliked=()=>{}}: IButtonProps) => {
+  return (
+    <button
+      className={style.Button + " DarkBackground"}
+      style={{ color: color }}
+      onClick={(evt)=>{
+        console.log(evt)
+        onCliked()
+      }}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;

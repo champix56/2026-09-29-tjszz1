89adca2143f763079b809439289dd20b47b15501
champix56 <desorbaix@free.fr>
2026-06-30 14:05:32 +0200

    navbar avec bootsrap

--- src/components/uis/Navbar/Navbar.tsx
+++ src/components/uis/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
 import type { FC } from "react";
 import styles from "./Navbar.module.css";
-
+import { Container, Nav, Navbar as NavBar } from "react-bootstrap";
+import  'bootstrap/dist/css/bootstrap.css'
+import '../../../../node_modules/bootstrap/dist/js/bootstrap.js'
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = () => (
+const Navbar: FC = () => (
   <div className={styles.Navbar} data-testid="Navbar">
+    <NavBar bg="dark" data-bs-theme="dark">
+      <Container>
+        <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </NavBar>
   </div>
 );
 

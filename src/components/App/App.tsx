920ef15c5fa610985e108435a59b30b49f793160
champix56 <desorbaix@free.fr>
2026-07-01 16:18:28 +0200

    passage par composants connectés

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,48 +4,19 @@ import Navbar from "../uis/Navbar/Navbar";
 import FlexH1Grow from "../layouts/FlexH1Grow/FlexH1Grow";
 import MemeForm from "../MemeForm/MemeForm";
 import Footer from "../uis/Footer/Footer";
+import  '../../store/store'
 import {
   emptyMeme,
-  MemeSVGViewer,
   type ImageInterface,
   type MemeInterface,
 } from "orsys-tjs-meme";
 import { useEffect, useState } from "react";
 import { RESSOURCES, REST_URL } from "../../config/constanteRest";
-import MemeThumbnail from "../uis/MemeThumbnail/MemeThumbnail";
+import MemeThumbnail from "../uis/MemeThumbnail/MemeThumbnail.stored";
+import MemeSVGViewer from "../uis/MemeSVGViewer/MemeSVGViewer";
 const App: React.FC = () => {
-  const [current, setCurrent] = useState(emptyMeme);
-  const [images, setimages] = useState<Array<ImageInterface>>([]);
-  const [memes, setmemes] = useState<Array<MemeInterface>>([]);
     console.log('mailto:desorbaix@free.fr?body='+JSON.stringify({abc:123}))
   console.log('%c%s','font-size:40pt;color:red;text-decoration:underline', 'ton boulot c\'est d\'utiliser lapp pas de nous chercher des poux')
-
-  useEffect(() => {
-    //setimages(imagesFromJSON);
-    fetch(`${REST_URL}/nawak`)
-    const pri=fetch(`${REST_URL}${RESSOURCES.images}`)
-      .then((resp) => resp.json())
-      //.then((arr) => setimages(arr));
-    const prm=fetch(`${REST_URL}${RESSOURCES.memes}`)
-      .then((resp) => resp.json())
-      //.then((arr) => setmemes(arr));
-
-    const timeOut=new Promise((resolved,rejeted)=>{
-      setTimeout(()=>{
-        resolved('TimeOut ecoulé')
-      },200)
-    })
-
-    const prAll=Promise.all([pri,prm])
-    Promise.race([prAll,timeOut]).then(resp=>{
-      if( typeof resp=='string'){console.log('error de request timeout')}
-      else {
-        setimages((resp as [ImageInterface[],MemeInterface[]] )[0])
-        setmemes((resp as [ImageInterface[],MemeInterface[]] )[1])
-      }
-    })
-
-  }, []);
 
   //const onMemeChange=(newCurrent:MemeInterface)=>{}
   return (
@@ -53,7 +24,7 @@ const App: React.FC = () => {
       <Header />
       <Navbar />
       <FlexH1Grow>
-        <MemeThumbnail memes={memes} images={images}/>
+        {/* <MemeThumbnail/> */}
         {/* <MemeSVGViewer
           meme={current}
           image={images.find((im) => im.id === current.imageId)}
@@ -66,6 +37,7 @@ const App: React.FC = () => {
             setCurrent(newCurrent);
           }}
         /> */}
+        <MemeSVGViewer/>
       </FlexH1Grow>
       <Footer />
     </FlexV3Grow>

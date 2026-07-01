351281aec83dc957db1cdb2fa409e1d1302b0921
champix56 <desorbaix@free.fr>
2026-07-01 17:49:06 +0200

    save current

--- src/store/asyncCaller/asyncRessources.ts
+++ src/store/asyncCaller/asyncRessources.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RESSOURCES, REST_URL } from "../../config/constanteRest";
+import type { MemeInterface } from "orsys-tjs-meme";
 
 export const initialRessourcesLoad = createAsyncThunk(
   "ressources/load",
@@ -8,5 +9,23 @@ export const initialRessourcesLoad = createAsyncThunk(
     const prm = fetch(`${REST_URL}${RESSOURCES.memes}`).then((r) => r.json());
     const prAll = await Promise.all([pri, prm]);
     return { images: await prAll[0], memes: await prAll[1] };
+  },
+);
+
+export const saveCurrent = createAsyncThunk(
+  "current/save",
+  async (meme: MemeInterface) => {
+    const pr = await fetch(
+      `${REST_URL}${RESSOURCES.memes}${meme.id !== undefined ? "/" + meme.id : ""}`,
+      {
+        method: meme.id !== undefined ? "PUT" : "POST",
+        body: JSON.stringify(meme),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    return { meme: await pr.json() };
   },
 );
--- src/store/slices/ressources.ts
+++ src/store/slices/ressources.ts
@@ -1,37 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { ImageInterface, MemeInterface } from 'orsys-tjs-meme';
-import * as data from '../../../db.json'
-import { initialRessourcesLoad } from '../asyncCaller/asyncRessources';
-interface IRessourcesState{
-    memes:Array<MemeInterface>
-    images:Array<ImageInterface>
+import { createSlice } from "@reduxjs/toolkit";
+import type { ImageInterface, MemeInterface } from "orsys-tjs-meme";
+import {
+  initialRessourcesLoad,
+  saveCurrent,
+} from "../asyncCaller/asyncRessources";
+interface IRessourcesState {
+  memes: Array<MemeInterface>;
+  images: Array<ImageInterface>;
 }
-export const initialState: IRessourcesState= {
-    memes:[],
-    images:[]
-
-}
+export const initialState: IRessourcesState = {
+  memes: [],
+  images: [],
+};
 
 const ressources = createSlice({
-  name: 'ressources',
+  name: "ressources",
   initialState,
   reducers: {
-    addSingleMeme:(state,action)=>{
-            state.memes.push(action.payload)
-            //anvcienne ecriture immutable return {...state,memes:[...state.memes,action.payload]}
-    }
+    addSingleMeme: (state, action) => {
+      state.memes.push(action.payload);
+      //anvcienne ecriture immutable return {...state,memes:[...state.memes,action.payload]}
+    },
   },
   extraReducers(builder) {
-      builder.addCase(initialRessourcesLoad.fulfilled, (s,a)=>{
-        s.images=a.payload.images
-        s.memes=a.payload.memes
-      })
+    builder.addCase(initialRessourcesLoad.fulfilled, (s, a) => {
+      s.images = a.payload.images;
+      s.memes = a.payload.memes;
+    });
+
+    builder.addCase(saveCurrent.fulfilled, (s, a) => {
+      const pos = s.memes.findIndex((m) => m.id === a.payload.meme);
+      if (pos === -1) s.memes.push(a.payload.meme);
+      else {
+        s.memes[pos] = a.payload.meme;
+      }
+    });
   },
 });
 
 //const addSingleMeme=(meme:MemeInterface)=>({type:'ressources/addSingleMeme',payload:meme})
 
-export const {addSingleMeme} = ressources.actions
+export const { addSingleMeme } = ressources.actions;
 
-const ressourcesReducer=ressources.reducer
-export default ressourcesReducer
+const ressourcesReducer = ressources.reducer;
+export default ressourcesReducer;
